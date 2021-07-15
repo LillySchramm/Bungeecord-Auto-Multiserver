a8229ec013facbb1c4131666d9085b1489517788
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2021-07-15 16:34:36 +0200

    Fixed an bug that caused disconnects not to be triggered (see comment)

--- Bungee/src/main/java/de/epsdev/plugins/bungee/events/PlayerDisconnectFromProxyEvent.java
+++ Bungee/src/main/java/de/epsdev/plugins/bungee/events/PlayerDisconnectFromProxyEvent.java
@@ -8,9 +8,15 @@ import net.md_5.bungee.event.EventHandler;
 // Renamed from 'PlayerDisconnectEvent' bc of net.md_5.bungee.api.event.PlayerDisconnectEvent
 public class PlayerDisconnectFromProxyEvent implements Listener {
 
+    /*
+            Note to self:
+                Even though the Bungeecord API is a nearly 1:1 copy of the Bukkit/Spigot API for some reason events need
+                to be public here. ( Differing on the use case, and alignment of the stars, a bukkit server might even
+                crash if served an 'public void' instead of just 'void' )
+     */
+
     @EventHandler
-    void onDisconnect(PlayerDisconnectEvent event){
+    public void onDisconnect(PlayerDisconnectEvent event){
         PlayerManager.onPlayerDisconnect(event.getPlayer().getDisplayName());
     }
-
 }
