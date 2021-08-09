2cedd0f00a8352c19c3d2f61d5341d4137e96bea
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2021-08-09 17:31:14 +0200

    Reenabled AutoUpdate

--- Spigot/src/main/java/de/epsdev/bungeeautoserver/spigottest/Bungeeautoserver/BungeecordAutoConfig.java
+++ Spigot/src/main/java/de/epsdev/bungeeautoserver/spigottest/Bungeeautoserver/BungeecordAutoConfig.java
@@ -67,12 +67,9 @@ public final class BungeecordAutoConfig extends JavaPlugin {
 
         // Updates
 
-        // TODO: Find out why this would throw an 403
-        // TEMP: Disabled till fix
-        //if(Config.isBungeeReady() && Config.checkUpdate("plugins/BungeecordAutoConfig",
-        //        "https://ci.eps-dev.de/job/BungeecordAutoConfig-Spigot/lastSuccessfulBuild/artifact/Spigot/target/sha512",
-        //        "https://ci.eps-dev.de/job/BungeecordAutoConfig-Spigot/lastSuccessfulBuild/artifact/Spigot/target/BungeecordAutoConfig.jar")){
-        if(Config.isBungeeReady()){
+        if(Config.isBungeeReady() && Config.checkUpdate("plugins/BungeecordAutoConfig",
+               "https://ci.eps-dev.de/job/BungeecordAutoConfig-Spigot/lastSuccessfulBuild/artifact/Spigot/target/sha512",
+                "https://ci.eps-dev.de/job/BungeecordAutoConfig-Spigot/lastSuccessfulBuild/artifact/Spigot/target/BungeecordAutoConfig.jar")){
             eps_api = new EPS_API(OperationType.CLIENT);
             eps_api.setRemoteAddress(config.getString("bungee_address"));
             eps_api.setPort(Bukkit.getPort());
