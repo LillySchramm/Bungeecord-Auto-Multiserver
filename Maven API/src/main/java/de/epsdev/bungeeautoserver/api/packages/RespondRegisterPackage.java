e9baa1453c561b6f85c7e93fd4406324ea0be7f2
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2021-10-09 19:27:32 +0200

    feat(maven_api): Add FTP Credentials To Register Protocol

--- Maven API/src/main/java/de/epsdev/bungeeautoserver/api/packages/RespondRegisterPackage.java
+++ Maven API/src/main/java/de/epsdev/bungeeautoserver/api/packages/RespondRegisterPackage.java
@@ -15,10 +15,20 @@ public class RespondRegisterPackage extends Package {
         super("RespondRegisterPackage");
 
         add("name", name);
+
+        add("ftp_server", EPS_API.ftpServerAddress);
+        add("ftp_port", EPS_API.ftpServerPort);
+        add("ftp_user", EPS_API.ftpServerUser);
+        add("ftp_password", EPS_API.ftpServerPassword);
     }
 
     @Override
     public void onPackageReceive(Socket socket, Object o) {
         EPS_API.NAME = getString("name");
+
+        EPS_API.ftpServerAddress = getString("ftp_server");
+        EPS_API.ftpServerPort = getInteger("ftp_port");
+        EPS_API.ftpServerUser = getString("ftp_user");
+        EPS_API.ftpServerPassword = getString("ftp_password");
     }
 }
